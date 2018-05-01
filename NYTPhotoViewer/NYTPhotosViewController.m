bd68c5dd95160d84151905a6edb145480fe0323e
Tiago Martinho <tiago@elit.software>
2018-05-01 15:38:00 +0200

    Pass either UIImage or NSData to Activity VC

--- NYTPhotoViewer/NYTPhotosViewController.m
+++ NYTPhotoViewer/NYTPhotosViewController.m
@@ -283,7 +283,7 @@ static const UIEdgeInsets NYTPhotosViewControllerCloseButtonImageInsets = {3, 0,
     }
     
     if (!clientDidHandle && (self.currentlyDisplayedPhoto.image || self.currentlyDisplayedPhoto.imageData)) {
-        NSData *image = self.currentlyDisplayedPhoto.image ? UIImagePNGRepresentation(self.currentlyDisplayedPhoto.image) : self.currentlyDisplayedPhoto.imageData;
+        id image = self.currentlyDisplayedPhoto.image ? self.currentlyDisplayedPhoto.image : self.currentlyDisplayedPhoto.imageData;
         UIActivityViewController *activityViewController = [[UIActivityViewController alloc] initWithActivityItems:@[image] applicationActivities:nil];
         activityViewController.popoverPresentationController.barButtonItem = sender;
         activityViewController.completionWithItemsHandler = ^(NSString * __nullable activityType, BOOL completed, NSArray * __nullable returnedItems, NSError * __nullable activityError) {
